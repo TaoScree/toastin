127f8e1e56ecf8baaa4bb34e18ffffa1eb5b6f13
Navyyyz <44703400+Navyyyz@users.noreply.github.com>
2020-03-20 04:30:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">    
     <meta name="site_name" content="toastin">
-    <meta name="author" content="toastin">
+    <meta name="author" content="Navyyy">
     <meta name="keywords" content="toastin, toasting, toast">
-    <meta name="image" content="https://toastin.me/toastin.gif">
+    <meta name="image" content="/toastin.gif">
     <meta name="url" content="https://toastin.me">
     <meta name="title" content="toastin">
     <meta name="description" content="toastin">
-    <meta name="og:site_name" content="Navyyy">
+    <meta name="og:site_name" content="toastin">
     <meta name="theme-color" content="#000080">
-    <meta name="og:image" content="https://toastin.me/toastin.gif">
+    <meta name="og:image" content="/toastin.gif">
     <meta name="og:url" content="https://toastin.me">
     <meta name="og:title" content="toastin">
     <meta name="og:description" content="toastin">
-    <link rel="icon" type="image/png" sizes="1000x1000" href="https://toastin.me/toastin.gif">
+    <link rel="icon" type="image/png" sizes="1000x1000" href="/toastin.gif">
 </head>
   <body background="toastin.gif">
 <center>
